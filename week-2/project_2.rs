67b61c23e1d4e3ef9e9f8cc7f0d9f49a5041191f
Stelllla-09 <stellamaris.ikegwuonu@pau.edu.ng>
2026-09-02 12:16:39 +0100

    Add project files for week-2

---
+++ week-2/project_2.rs
@@ -0,0 +1,20 @@
+fn main () {
+	let toshiba_amount:f64 = 450_000.0;
+	let mac_amount:f64 = 1_500_000.0;
+	let hp_amount:f64 = 750_000.0;
+	let dell_amount:f64 = 2_850_000.0;
+	let acer_amount:f64 = 250_000.0;
+
+ let toshiba_qty:f64 = 2.0;
+	let mac_qty:f64 = 1.0;
+	let hp_qty:f64 = 3.0;
+	let dell_qty:f64 = 3.0;
+	let acer_qty:f64 =1.0;
+
+ let sum = toshiba_amount + mac_amount + hp_amount + dell_amount + acer_amount;
+ let no = toshiba_qty + mac_qty + hp_qty + dell_qty + acer_qty;
+let average = sum / no;
+
+println!("Sum of Amounts is {}", sum);
+	println!("Average is {}", average);
+}
